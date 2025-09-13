a29719cd3977a082c1c9fd4a4929984ca3ae1c4e
LautiZ <zullolau@gmail.com>
2025-09-13 19:09:03 -0300

    Tp5 - Entrega ejercicio 5 'UML'.

---
+++ Tp5/src/ej5/Computadora.java
@@ -0,0 +1,64 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ej5;
+
+/**
+ *
+ * @author zullo
+ */
+public class Computadora {
+    private String marca;
+    private String nroSerie;
+    private PlacaMadre placaMadre;
+    private Propietario propietario;
+
+    public Computadora(String marca, String nroSerie, PlacaMadre placaMadre) {
+        this.marca = marca;
+        this.nroSerie = nroSerie;
+        this.placaMadre = placaMadre;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getNroSerie() {
+        return nroSerie;
+    }
+
+    public void setNroSerie(String nroSerie) {
+        this.nroSerie = nroSerie;
+    }
+
+    public PlacaMadre getPlacaMadre() {
+        return placaMadre;
+    }
+
+    public void setPlacaMadre(PlacaMadre placaMadre) {
+        this.placaMadre = placaMadre;
+    }
+
+    public Propietario getPropietario() {
+        if (propietario != null) {
+            return propietario;
+        } else {
+            return null;
+        }
+    }
+
+    public void setPropietario(Propietario propietario) {
+        this.propietario = propietario;
+        
+        if (propietario != null) {
+            propietario.setComputadora(this); // Llama a setCoche en Matricula
+        }
+    }
+    
+    
+}
