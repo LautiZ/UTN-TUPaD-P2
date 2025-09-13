34a4c0abafb050778be6c8a6281400d9d140efa2
LautiZ <zullolau@gmail.com>
2025-09-13 15:13:42 -0300

    Tp5 - Entrega ejercicio 2 'UML'.

---
+++ Tp5/src/ej2/Usuario.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ej2;
+
+/**
+ *
+ * @author zullo
+ */
+public class Usuario {
+    private String nombre;
+    private String dni;
+
+    public Usuario(String nombre, String dni) {
+        this.nombre = nombre;
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+    
+    
+}
